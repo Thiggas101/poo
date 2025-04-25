9840e66e2e5f3157150085cb4e445706a6bf41d7
Thiggas101 <thiagodellano689@gmail.com>
2025-04-25 09:04:31 -0300

    thiago-dellano-Av1

---
+++ challenges/03-parking/Thiago-Dellano/Motorcycle.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(String plate, String brand, String model) {
+        super(plate, brand, model);
+    }
+
+    @Override
+    public double calculateParkingFee(int hours) {
+        return hours * 3.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto - " + super.toString();
+    }
+}
